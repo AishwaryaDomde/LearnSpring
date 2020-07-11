e2b8cd7d757bdda18a4ee6b1300a057d6b85a171
AishwaryaDomde <50988287+AishwaryaDomde@users.noreply.github.com>
2020-07-11 19:20:56 +0530

    Add files via upload

---
+++ ServletALL/src/Src/Servlet01.java
@@ -0,0 +1,55 @@
+package Src;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet01
+ */
+@WebServlet("/Servlet01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet01() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  response.setContentType("text/html;charset=UTF-8");
+	      PrintWriter out = response.getWriter();
+	      String name=request.getParameter("name");
+	            /* TODO output your page here. You may use following sample code. */
+	            out.println("<!DOCTYPE html>");
+	            out.println("<html>");
+	            out.println("<head>");
+	            out.println("<title>Hello"+name+"</title>");            
+	            out.println("</head>");
+	            out.println("<body>");
+	            out.println("<h1>Servlet Servlet01 at " + name + "</h1>");
+	            out.println("</body>");
+	            out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
